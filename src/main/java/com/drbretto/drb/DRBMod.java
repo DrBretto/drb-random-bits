e338870e86a64e396d2033106eb0b98928039785
Brett Westerlund <drbretto82@gmail.com>
2021-08-07 13:52:54 -0400

    Added String for MOD_ID - tested intelliJ git buttons

--- src/main/java/com/drbretto/drb/DRBMod.java
+++ src/main/java/com/drbretto/drb/DRBMod.java
@@ -3,6 +3,9 @@ package com.drbretto.drb;
 import net.fabricmc.api.ModInitializer;
 
 public class DRBMod implements ModInitializer {
+
+	public static final String MOD_ID = "drb";
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
