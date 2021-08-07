54c8fca4e291ae2eebbc265e37fcfb66c59c60f1
Brett Westerlund <drbretto82@gmail.com>
2021-08-07 14:41:48 -0400

    Added Creeper_heart to registry

--- src/main/java/com/drbretto/drb/DRBMod.java
+++ src/main/java/com/drbretto/drb/DRBMod.java
@@ -1,16 +1,23 @@
 package com.drbretto.drb;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class DRBMod implements ModInitializer {
 
 	public static final String MOD_ID = "drb";
 
+	public static final Item CREEPER_HEART = new CreeperHeartItem(new Item.Settings().group(ItemGroup.MATERIALS).maxCount(32));
+
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+
+		Registry.register( Registry.ITEM, new Identifier(MOD_ID, "creeper_heart"), CREEPER_HEART);
+
+
 
 		System.out.println("Hello Fabric world!");
 	}
