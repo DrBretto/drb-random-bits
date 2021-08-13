2ab44f677aba9cc50d6f1d791993a28c3efa3dcf
Brett Westerlund <drbretto82@gmail.com>
2021-08-12 20:16:14 -0400

    Boom Stick fully functional

--- src/main/java/com/drbretto/drb/DRBMod.java
+++ src/main/java/com/drbretto/drb/DRBMod.java
@@ -28,7 +28,7 @@ public class DRBMod implements ModInitializer {
 	public static final ItemGroup GROUP = FabricItemGroupBuilder.build(new Identifier("drb", "group"), () -> new ItemStack(DRBMod.CREEPER_HEART));
 
 	public static final Item CREEPER_HEART = new CreeperHeartItem(new Item.Settings().group(GROUP).maxCount(32));
-	//public static final Item BOOM_STICK = new BoomStick(12f, 12f, 2, );
+	public static final Item BOOM_STICK = new BoomStick(new Item.Settings().group(GROUP));
 
 	public static final Block GUNPOWDER_BLOCK = new GunpowderBlock(FabricBlockSettings.of(Material.ORGANIC_PRODUCT).luminance(15));
 	public static final Item GUNPOWDER_BLOCK_ITEM = new BlockItem(GUNPOWDER_BLOCK, new Item.Settings().group(GROUP));
@@ -44,7 +44,7 @@ public class DRBMod implements ModInitializer {
 	public void onInitialize() {
 
 		Registry.register( Registry.ITEM, new Identifier(MOD_ID, "creeper_heart"), CREEPER_HEART);
-
+		Registry.register( Registry.ITEM, new Identifier(MOD_ID, "boom_stick"), BOOM_STICK);
 		Registry.register( Registry.ITEM, new Identifier(MOD_ID, "gunpowder_block"), GUNPOWDER_BLOCK_ITEM);
 
 		Registry.register( Registry.BLOCK, new Identifier(MOD_ID, "gunpowder_block"), GUNPOWDER_BLOCK);
