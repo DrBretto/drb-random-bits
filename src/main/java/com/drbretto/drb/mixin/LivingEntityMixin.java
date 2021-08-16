78b6cf3b113bcad630307499814e140ca08e1cdd
Brett Westerlund <drbretto82@gmail.com>
2021-08-16 14:49:40 -0400

    implemented LivingEntityMixin - spawns explosion when on fire

---
+++ src/main/java/com/drbretto/drb/mixin/LivingEntityMixin.java
@@ -0,0 +1,70 @@
+package com.drbretto.drb.mixin;
+
+import com.drbretto.drb.DRBMod;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.TntEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LivingEntity.class)
+
+
+public abstract class LivingEntityMixin extends Entity {
+
+    @Shadow @Final private DefaultedList<ItemStack> equippedArmor;
+
+    public LivingEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
+    @Inject(at = @At("HEAD"),method="tick")
+    private void tick(CallbackInfo ci){
+
+        if(isOnFire()) {
+            ItemStack helmetStack = equippedArmor.get(3);
+            ItemStack chestplateStack = equippedArmor.get(2);
+            ItemStack leggingsStack = equippedArmor.get(1);
+            ItemStack bootsStack = equippedArmor.get(0);
+
+            int explosionCount = 0;
+
+            if(helmetStack.getItem().equals(DRBMod.GUNPOWDER_HELMET)){
+                explosionCount++;
+            }
+            if(chestplateStack.getItem().equals(DRBMod.GUNPOWDER_CHESTPLATE)){
+                explosionCount++;
+            }
+            if(leggingsStack.getItem().equals(DRBMod.GUNPOWDER_LEGGINGS)){
+                explosionCount++;
+            }
+            if(bootsStack.getItem().equals(DRBMod.GUNPOWDER_BOOTS)){
+                explosionCount++;
+            }
+
+            System.out.println("Standing in fire" + helmetStack);
+
+            if(explosionCount > 0){
+
+
+
+                for (int i =0; i < explosionCount; i++) {
+                    TntEntity tntEntity = EntityType.TNT.create(world);
+                    tntEntity.setFuse(0);
+                    tntEntity.setPos(this.prevX, this.prevY, this.prevZ);
+
+                    world.spawnEntity(tntEntity);
+                }
+            }
+        }
+
+    }
+}
